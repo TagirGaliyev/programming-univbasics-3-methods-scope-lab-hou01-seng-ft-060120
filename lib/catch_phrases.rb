8ef91668e86d302916b5c6249a127a941fb5baf2
Tagir Galiyev <w.tagir.galiyev@gmail.com>
2020-05-12 21:30:06 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,20 +1,21 @@
 $phrase = "It's-a me, Mario!"
 $status = "Thank You Mario! But Our Princess Is In Another Castle!"
 $link_phrase = "It's Dangerous To Go Alone! Take This."
+
 def mario
-  puts phrase
+  puts $phrase
 end
 
 def toadstool
-  puts status
+  puts $status
 end
 
 def link
-  puts link_phrase
+  puts $link_phrase
 end
 
 def all_phrases
-  puts phrase
-  puts status
-  puts link_phrase
+  puts $phrase
+  puts $status
+  puts $link_phrase
 end
