2673574dc5d522c4f2c971f3255aca73d4bb3e96
Tagir Galiyev <w.tagir.galiyev@gmail.com>
2020-05-12 21:24:34 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,12 +1,8 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
 def toadstool
   puts status
-end
-
-def mario
-  puts "It's-a me, Mario!"
 end
